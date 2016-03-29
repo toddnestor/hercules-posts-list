ca9b7bfb50104fd5148d81d3f504686d19e01ad6
Todd Nestor <todd.nestor@gmail.com>
2016-03-29 11:51:01 -0700

    fixed one other area

--- hercules-framework/core/View.php
+++ hercules-framework/core/View.php
@@ -419,7 +419,7 @@ class View extends HercAbstract
 		{
 			case 'all':
 				$post_types = get_post_types( '', 'names' );
-				$exclude_types = [ 'attachment', 'revision', 'nav_menu_item' ];
+				$exclude_types = array( 'attachment', 'revision', 'nav_menu_item' );
 				foreach( $post_types as $type )
 				{
 					if( !in_array( $type, $exclude_types ) )
